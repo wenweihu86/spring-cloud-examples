ac33d46ead157700f7884febc47f699b004891cf
wenweihu86 <wenweihu86@163.com>
2019-05-23 19:29:24 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # spring-cloud-examples
 spring cloud高可用集群示例，包括注册中心、配置中心、网关、feign使用。
+
+# quick start
+* sh deploy.sh
+* 访问http://localhost:8890/可以看到eureka集群成功启动。
+* 访问http://localhost:8887/inventory-server-dev.properties查看配置中心的配置。
+* 访问http://localhost:8085/data/order/create查看服务是否正常访问。
+
--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 mvn clean package -DskipTests
 
-nohup java -Dspring.profiles.active=peer1 -jar eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar &
-nohup java -Dspring.profiles.active=peer2 -jar eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar &
+nohup java -Dspring.profiles.active=peer1 -jar eureka-server/target/eureka-server.jar &
+nohup java -Dspring.profiles.active=peer2 -jar eureka-server/target/eureka-server.jar &
 
-nohup java -Dspring.profiles.active=peer1 -jar config-server/target/config-server-0.0.1-SNAPSHOT.jar &
-nohup java -Dspring.profiles.active=peer2 -jar config-server/target/config-server-0.0.1-SNAPSHOT.jar &
+nohup java -Dspring.profiles.active=peer1 -jar config-server/target/config-server.jar &
+nohup java -Dspring.profiles.active=peer2 -jar config-server/target/config-server.jar &
 
-nohup java -jar inventory-server/target/inventory-server-0.0.1-SNAPSHOT.jar &
+nohup java -jar inventory-server/target/inventory-server.jar &
 
-nohup java -jar order-server/target/order-server-0.0.1-SNAPSHOT.jar &
+nohup java -jar order-server/target/order-server.jar &
 
-nohup java -jar gateway/target/gateway-0.0.1-SNAPSHOT.jar &
+nohup java -jar gateway/target/gateway.jar &
